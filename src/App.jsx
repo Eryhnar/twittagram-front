005f9650f7e89d7b5722e5423ff8d6caa56f8d4b
pfernandez <pedrofernbel@gmail.com>
2024-04-06 17:30:16 +0200

    feat: created SideNav component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
+import { SideNav } from './common/SideNav/SideNav'
 import { Body } from './pages/Body/Body'
 
 function App() {
 
-  return (
-    <>
-      <Body />
-    </>
-  )
+    return (
+        <div className="App-content-grid">
+            <Body />
+            <SideNav />
+        </div>
+    )
 }
 
 export default App
