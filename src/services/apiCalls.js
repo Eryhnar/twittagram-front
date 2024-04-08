c4ada414cc2fa30d31b5b9aa5d50066afdea7333
pfernandez <pedrofernbel@gmail.com>
2024-04-08 16:55:13 +0200

    feat: added LoginService to apiCalls.

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,27 +1,45 @@
 const root = "http://localhost:4000/api/";
 
 export const RegisterService = async (user) => {
-    // try {
-        const response = await fetch(root + "auth/register", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(user),
-        });
+    const response = await fetch(root + "auth/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+    });
 
-        const parsedResponse = await response.json();
+    const parsedResponse = await response.json();
 
-        if (response.status === 404) {
-            throw new Error("Could not connect to server");
-        }
+    if (response.status === 404) {
+        throw new Error("Could not connect to server");
+    }
 
-        if (response.status !== 201) {
-            throw new Error(parsedResponse.message);
-        }
+    if (response.status !== 201) {
+        throw new Error(parsedResponse.message);
+    }
 
-        return parsedResponse;
-    // } catch (error) {
-        // throw error;
-    // }
+    return parsedResponse;
+}
+
+export const LoginService = async (credentials) => {
+    const response = await fetch(root + "auth/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+    });
+
+    const parsedResponse = await response.json();
+
+    if (response.status === 404) {
+        throw new Error("Could not connect to server");
+    }
+
+    if (response.status !== 200) {
+        throw new Error(parsedResponse.message);
+    }
+
+    return parsedResponse;
 }
