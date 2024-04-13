f73e97c3fa7926ebf0969aaf42b94c338024bcfb
pfernandez <pedrofernbel@gmail.com>
2024-04-13 13:06:42 +0200

    feat: implemented cloudinary widget that returns constructed url on upload.

---
+++ src/common/UploadWidget/UploadWidget.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from "react"
+import { CButton } from "../CButton/CButton";
+
+export const UploadWidget = ({ onUploadSuccess }) => {
+    const cloudinaryRef = useRef();
+    const widgetRef = useRef();
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        if (isOpen) {
+            cloudinaryRef.current = window.cloudinary;
+            widgetRef.current = cloudinaryRef.current.createUploadWidget({
+                cloudName: "dib7jxktr",
+                uploadPreset: "ml_default"
+            }, (error, result) => {
+                if (!error && result.info.public_id) {
+                    onUploadSuccess(result.info.public_id, result.info.format);
+                    // console.log("Done! Here is the image info: ", result.info);
+                } else if (error){
+                    throw new Error("Error uploading image: ", error);
+                }
+            })
+            widgetRef.current.open();
+        }
+        return () => {
+            if (widgetRef.current) {
+                widgetRef.current.close();
+            }
+        }
+    }, [isOpen]);
+
+    const openWidget = () => {
+        setIsOpen(true);
+    }
+
+    return (
+        <CButton 
+            className={"upload-widget-button"}
+            title={"Upload Image"}
+            onClickFunction={() => openWidget()}
+        />
+    )
+}
--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,7 +1,24 @@
+import { UploadWidget } from "../../common/UploadWidget/UploadWidget"
+import { useEffect, useState } from "react";
+
 export const CreatePost = () => {
+    const [imageUrl, setImageUrl] = useState("");
+
+    const constructImageURL = (publicId, fileExtension) => {
+        const url = `https://res.cloudinary.com/dib7jxktr/image/upload/${publicId}.${fileExtension}`
+        setImageUrl(`https://res.cloudinary.com/dib7jxktr/image/upload/${publicId}.${fileExtension}`)
+        // return url;
+    }
+
+    useEffect(() => {
+        console.log("Image URL: ", imageUrl);
+    }, [imageUrl])
+    
     return (
         <div>
-            Create Post
+            <UploadWidget 
+                onUploadSuccess={constructImageURL}
+            />
         </div>
     )
 }
